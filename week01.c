104b0b0ce02c3159927dd49856276995b4999d14
haksu-kim <80704929+haksu-kim@users.noreply.github.com>
2025-03-13 07:51:07 +0900

    Update

--- week01.c
+++ week01.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void Bit(unsigned int i) {
-    for(int k=31; k>=0; k--) {
-        printf("%d", (i>>k) & 1);
+void Bit(unsigned int i) { //Bit함수 생성
+    for(int k=31; k>=0; k--) { //k가 31부터 -1씩 감소하며 0까지 printf 반복
+        printf("%d", (i>>k) & 1); // 비트를 추출하면서 추출 된 비트값을 오른쪽으로 k만큼 밀어낸다
     }
 }
 
 int main() {
     int i;
     printf("정수를 입력하세요 : ");
-    scanf("%d", &i);
-    Bit(i);
+    scanf("%d", &i); //정수값 받기
+    Bit(i); // Bit함수에 값 대입
     return 0;
 }
